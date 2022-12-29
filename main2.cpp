31dd4638d6c98247e8e7375170638b1a095952ac
pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2022-12-29 21:48:03 +0300

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main(){
+int a,b;
+
+printf("Enter 2 numbers:");
+printf("Number 1 = ");
+scanf("%i",&a);
+printf("Number 2 = ");
+scanf("%i",&b);
+       
+               if(a>b){
+               printf("Number 1 > Number 2");
+               }
+               else{
+               if(a==b){
+               printf("Number 1 = Number 2");
+               }
+               else{  
+               printf("Number 1 < Number 2");
+               }
+               }
+               }
+                  
