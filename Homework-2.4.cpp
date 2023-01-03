6a8338363364e15230c2568c0454730b4a016179
pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-01-03 19:59:48 +0300

    Add files via upload

---
+++ Homework-2.4.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+
+int day;
+
+main(){
+       
+       printf("Enter day...");
+       scanf("%i",&day);
+       
+       switch(day){
+              
+              case 1:{
+                   printf("Monday");
+                   break;
+                   }
+              case 2:{
+                   printf("Tuesday");
+                 break;
+                   }
+              case 3:{
+                   printf("Wensday");
+                   break;
+                   }
+              case 4:{
+                   printf("Thursday");
+                  break;
+                   }
+              case 5:{
+                   printf("Friday");
+                  break;
+                   }
+              case 6:{
+                   printf("Saturday");
+                   break;
+                   }
+              case 7:{
+                   printf("Sunday");
+                   break;
+                   }
+                   default:
+                           {
+                           printf("Error");
+                           }
+                   
+              
+              }
+       
+       getch();
+       }
