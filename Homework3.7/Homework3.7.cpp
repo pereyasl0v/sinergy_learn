4bc230b323744687f88fa87092179d0e35f3af5b
pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-01-04 22:44:05 +0300

    Add files via upload

---
+++ Homework3.7/Homework3.7.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int i, j=0;
+
+main(){
+       
+      for(i=1;i<101;i++){
+      
+      j=j+i;
+      
+      printf("%i \n", j);
+      }
+       getch();
+       }
