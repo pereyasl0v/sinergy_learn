e5abedc3ae7158b79c418795ea4db69fa0661724
pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-01-04 22:42:45 +0300

    Add files via upload

---
+++ Homewrk3.5/2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+       
+       int a=5;
+       int b=5;
+       
+       while(a<16){
+               
+      printf("%i\n",b);   
+       a=a+1;      
+       b=b+a;
+               }
+               
+       getch();
+       }
