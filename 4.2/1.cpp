5a0c4139f139ee5cbb505e599e012e5af83b456c
pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-01-05 14:34:55 +0300

    Add files via upload

---
+++ 4.2/1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+       
+       int mas[7];
+       int i;
+       
+       for(i=0;i<7;i++){
+                        printf("Enter number...");
+                        scanf("%i", &mas[i]);
+                        
+                        }
+             for(i=0;i<7;i++){
+                        printf("%2i", mas[i]);
+                        
+                        }
+       getch();
+       }
