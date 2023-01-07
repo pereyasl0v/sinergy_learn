5a54d3cf3c0f1f13f748e3ceb90345a1194d4abe
pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-01-07 14:08:07 +0300

    Add files via upload

---
+++ 6.4/1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+main(){
+       
+
+       char str[100];
+
+       gets(str);
+      
+
+for(int n=0;n<strlen(str);n++){
+      str[n]=str[n+1];
+      }
+      
+       puts(str);
+
+       
+                     getch();
+       
+       }
